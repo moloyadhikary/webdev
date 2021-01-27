d7ccf474f95e3143c9a13bf01b3742330ab9185a
Moloy Adhikary <moloy_a@hotmail.com>
2021-01-27 11:16:10 +0600

    first commit

---
+++ OopIntroduction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            Console.WriteLine(age);
+
+            age = 20;
+            Console.WriteLine(age);
+            
+
+            var abc = Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
