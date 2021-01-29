5c04fa7c29db2609d15fd9f76b46a36efbc5b5f2
Moloy Adhikary <moloy_a@hotmail.com>
2021-01-30 00:27:51 +0600

    210129

---
+++ OopIntroduction/IfElse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopIntroduction
+{
+    class IfElse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are above 18?");
+            string userInput = Console.ReadLine();
+             
+            if (userInput == "Yes")
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else if (userInput == "Y")
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else if (userInput == "y")
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else
+            {
+                Console.WriteLine("Hello kid!");
+            }
+
+
+
+
+
+            string abc = Console.ReadLine();
+        }
+    }
+}
--- OopIntroduction/Program.cs
+++ OopIntroduction/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,28 @@ namespace OopIntroduction
 {
     class Program
     {
-        static void Main(string[] args)
+        static void MainOld(string[] args)
         {
             int age = 10;
-            Console.WriteLine(age);
+            //Console.WriteLine(age);
 
             age = 20;
-            Console.WriteLine(age);
-            
+            //Console.WriteLine(age);
 
+            Student obj = new Student();
+            int c = obj.Add(10, 20);
+            //Console.Write(c);
+
+            int d = obj.CalculateYear(1976);
+            //Console.WriteLine(d);
+                
+            //Console.WriteLine("Enter your name: ");
+            //string abc = Console.ReadLine();
+            Console.WriteLine("Enter your birth year: ");
+            string userAge = Console.ReadLine();
+            var userAgeC = obj.CalculateYear(Convert.ToInt32(userAge));
+            Console.WriteLine("Year age is: " + userAgeC.ToString() + " years");
+            
             var abc = Console.ReadLine();
         }
     }
@@ -30,7 +44,16 @@ namespace OopIntroduction
 
         public int Add(int a, int b)
         {
-            return a + b;
+            int addedValue = a + b;
+            int dev = addedValue / 2;
+            return dev;
+        }
+
+        public int CalculateYear(int year)
+        {
+            int current = DateTime.Now.Year;
+            int result = current - year;
+            return result;
         }
     }
 }
