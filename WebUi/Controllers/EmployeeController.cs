3512ca8a4a13630187c5df4356d67ace1a61ea2d
Moloy Adhikary <moloy_a@hotmail.com>
2021-03-08 23:46:58 +0600

    20210308

---
+++ WebUi/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUi.Models;
+
+namespace WebUi.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult List()
+        {
+            Employee objEmployee = new Employee();
+            var employeeList = objEmployee.GetEmployee();
+            return View(employeeList);
+        }
+    }
+}
---
+++ WebUi/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeId { get; set; }
+        public string FullName { get; set; }
+
+        public List<Employee> GetEmployee()
+        {
+            var list = new List<Employee>();
+
+            list.Add(new Employee{Id = 1, EmployeeId = "ABC123", FullName = "Moloy"});
+            list.Add(new Employee{Id = 2, EmployeeId = "ABC321", FullName = "Shabab"});
+
+            return list;
+        }
+    }
+}
